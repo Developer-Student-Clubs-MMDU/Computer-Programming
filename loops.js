2842d31dcacd29b1bd4163b578a5df13f412636d
Omkar Date <omkardate29@gmail.com>
2022-10-15 20:17:59 +0530

    Create loops.js

---
+++ loops.js
@@ -0,0 +1,19 @@
+// Loops in JavaScript
+
+const n = 5;
+let i = 0;
+for (let i = 1; i <= n; i++) {
+  console.log("I love JavaScript");
+}
+
+while (i < n) {
+  console.log("I love JavaScript");
+  i += 1;
+}
+
+i = 0;
+
+do {
+  console.log("I love JavaScript");
+  i += 1;
+} while (i < n);
